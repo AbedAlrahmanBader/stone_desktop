50852c3a58ff0f5f187abb61143ed159f612f6f7
AbedAlrahmanBader <abedalrahman.bader0@gmail.com>
2026-07-24 21:54:26 +0300

    Update project

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://server-stone.onrender.com/api"
+    baseURL: "https://server-stone-1.onrender.com/api"
 });
 
 export default api;
