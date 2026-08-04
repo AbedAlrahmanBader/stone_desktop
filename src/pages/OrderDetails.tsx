c671db1dd71775397377ab13291d115e0d1c661c
AbedAlrahmanBader <abedalrahman.bader0@gmail.com>
2026-08-04 19:31:54 +0300

    Update project

--- src/pages/OrderDetails.tsx
+++ src/pages/OrderDetails.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import api from "../api/axios";
-import "../styles/orderDetails.css";
+import "../styles/OrderDetails.css";
 
 function OrderDetails() {
   const { orderNumber } = useParams();
